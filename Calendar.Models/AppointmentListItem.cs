55628c12b39d1492f17a8645ae7022632b4ede2d
daniellebarrett <daniellebarrett@alumni.iu.edu>
2021-04-10 13:46:42 -0400

    Added AppointmentListItem and Index View

---
+++ Calendar.Models/AppointmentListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.Models
+{
+    public class AppointmentListItem
+    {
+        [Key]
+        public int AppointmentID { get; set; }
+        [Required]
+        [Display(Name = "Date of Desired Appointment")]
+        public DateTimeOffset AppointmentDate { get; set; }
+    }
+}
---
+++ Calendar.WebMVC/Controllers/AppointmentController.cs
@@ -0,0 +1,20 @@
+﻿using Calendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calendar.WebMVC.Controllers
+{
+    [Authorize]
+    public class AppointmentController : Controller
+    {
+        // GET: Appointment
+        public ActionResult Index()
+        {
+            var model = new AppointmentListItem[0];
+            return View(model);
+        }
+    }
+}
