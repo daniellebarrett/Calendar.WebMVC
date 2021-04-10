ed86a764666c69a771328c0e219aed35a7ab52f1
daniellebarrett <daniellebarrett@alumni.iu.edu>
2021-04-10 13:27:43 -0400

    added properties to Appointment class

---
+++ Calendar.Data/Appointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.Data
+{
+    public class Appointment
+    {
+        [Key]
+        public int AppointmentID { get; set; }
+        [Required]
+        [Display(Name = "Date of Desired Appointment")]
+        public DateTimeOffset AppointmentDate { get; set; }
+        [Required]
+        [Display(Name = "Start Time of Desired Appointment")]
+        public DateTimeOffset StartTime { get; set; }
+        [Required]
+        [Display(Name = "End Time of Desired Appointment")]
+        public DateTimeOffset EndTime { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string AppointmentReason { get; set; }
+    }
+}
