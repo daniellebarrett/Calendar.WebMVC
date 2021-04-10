401104560583e1addb186daac89ecaae26172781
daniellebarrett <daniellebarrett@alumni.iu.edu>
2021-04-10 13:14:01 -0400

    Add project files.

---
+++ Calendar.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Calendar.WebMVC.MvcApplication" Language="C#" %>
---
+++ Calendar.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Calendar.WebMVC.Startup))]
+namespace Calendar.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
